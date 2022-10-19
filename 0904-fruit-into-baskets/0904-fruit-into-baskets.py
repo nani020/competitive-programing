97636c6787527c0b34640c0ce065e83e4a1d5063
nani020 <56104039+nani020@users.noreply.github.com>
2022-10-19 13:03:09 +0300

    Time: 2684 ms (14.18%), Space: 20.1 MB (49.99%) - LeetHub

---
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -0,0 +1,16 @@
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        basket = {}
+        max_picked = 0
+        left = 0
+        for right in range(len(fruits)):
+            basket[fruits[right]] = basket.get(fruits[right], 0) + 1
+            while len(basket) > 2:
+                basket[fruits[left]] -= 1
+                if basket[fruits[left]] == 0:
+                    del basket[fruits[left]]
+                left += 1
+            max_picked = max(max_picked, right - left + 1)
+        
+        # Return max_picked as the maximum number of fruits we can collect.
+        return max_picked
